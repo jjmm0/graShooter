066f8e65d2a854941fa05c4b213fbccb25775cf9
jjmm0 <jedrzej.miga@gmail.com>
2020-08-28 13:38:21 +0200

    Gra

---
+++ components/oknoGry.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="wrapper">
+    <canvas id="game"></canvas>
+  </div>
+</template>
+<script>
+
+export default {
+  data(){
+    return{
+      player: {
+        x: 10,
+        y: 10,
+        xSpeed: 1,
+        ySpeed: 1
+      },
+      speed: 0.1,
+      wPressed: false,
+      sPressed: false,
+      aPressed: false,
+      dPressed: true,
+      upPressed: false,
+      downPressed: false,
+      leftPressed: false,
+      rightPressed: false,
+      SPEED: 1,
+    // canvas: this.$refs.canvas,
+    // context: this.canvas.getContext(),
+    }
+  },
+  computed:{
+
+  },
+  mounted(){
+    const canvas = document.getElementById('#game');
+    debugger
+    const context = canvas.getContext('2d');
+    this.animate()
+  },
+  methods:{
+    animate() {
+      // if (wPressed) {
+      //     player.ySpeed = player.ySpeed - SPEED;
+      // }
+      // if (sPressed) {
+      //     player.ySpeed = player.ySpeed + SPEED;
+      // }
+      let {wPressed, sPressed, aPressed, dPressed, player, SPEED, canvas, context} = this 
+      if (wPressed) {player.ySpeed = player.ySpeed - SPEED;}
+      if (sPressed) {player.ySpeed = player.ySpeed + SPEED;}
+      if (aPressed) {player.xSpeed = player.xSpeed - SPEED;}
+      if (dPressed) {player.xSpeed = player.xSpeed + SPEED;}
+
+      
+      player.x = player.x + player.xSpeed;
+      player.y = player.y + player.ySpeed;
+
+      // context.clearRect(0, 0, canvas.width, canvas.height);    
+      // context.fillRect(player.x, player.y, 10, 10);
+      debugger
+      if (player.x > canvas.width) {
+          player.xSpeed = -player.xSpeed;
+          player.x = canvas.width - 1;
+      }
+      if (player.y > canvas.height) {
+          player.ySpeed = -player.ySpeed;
+          player.y = canvas.height - 1;
+      }
+      if (player.x < 0) {
+          player.xSpeed = -player.xSpeed;
+          player.x = 1;
+      }
+      if (player.y < 0) {
+          player.ySpeed = -player.ySpeed;
+          player.y = 1;
+      }
+
+      requestAnimationFrame(() => {
+          this.animate();
+      });
+}
+  },
+
+}
+</script>
+<style scoped>
+@import '~/assets/styles/oknoGryStyles.css';
+</style>
